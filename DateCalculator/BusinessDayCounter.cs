f212c41801ce6501ea7a0b81f53897343e46b3b4
Eric Studley <studley11@live.com.au>
2023-10-03 18:45:50 +1100

    Bug fixes  - Don't count the supplied dates Added businessday counter

--- DateCalculator/BusinessDayCounter.cs
+++ DateCalculator/BusinessDayCounter.cs
@@ -12,16 +12,29 @@ namespace DateCalculator
         {
             if (secondDate <= firstDate) return 0;
 
-            var totalDays = (secondDate - firstDate).TotalDays;
-            var numberOfWeekends = (totalDays + (int) firstDate.DayOfWeek) / 7 * 2;
+            var totalDays = (int) (secondDate - firstDate).TotalDays - 1;
+            var numberOfWeekends = (int) (totalDays + (int) firstDate.DayOfWeek + 1) / 7 * 2;
 
-            return (int) (totalDays - numberOfWeekends);
-            //todo
+            if (firstDate.DayOfWeek == DayOfWeek.Saturday) numberOfWeekends--;
+            if (secondDate.DayOfWeek == DayOfWeek.Sunday) numberOfWeekends--;
+
+            return totalDays - numberOfWeekends;
         }
         public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
         {
-            //todo
-            return 0;
+            if (secondDate <= firstDate) return 0;
+
+            var totalDays = WeekdaysBetweenTwoDates(firstDate, secondDate);
+
+            foreach (var holiday in publicHolidays)
+            {
+                if (firstDate < holiday && holiday < secondDate && holiday.DayOfWeek != DayOfWeek.Saturday && holiday.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    totalDays--; 
+                }
+            }
+
+           return totalDays;
         }
     }
 }
--- DateCalculator/BusinessDayCounterTests.cs
+++ DateCalculator/BusinessDayCounterTests.cs
@@ -17,18 +17,20 @@ namespace DateCalculator
 
             var result = counter.WeekdaysBetweenTwoDates(startDate, endDate);
 
-            Assert.Equal(result, expectedResult);
+            Assert.Equal(expectedResult, result);
         }
 
         [Theory]
         [ClassData(typeof(BusinessDaysTestData))]
-        public void BusinessDaysBetweenTwoDatesTests(DateTime startDate, DateTime endDate, IList<DateTime> publicHolidays, int expectedResult)
+        public void BusinessDaysBetweenTwoDatesTests(DateTime startDate, DateTime endDate, int expectedResult)
         {
+            var publicHolidays = new List<DateTime> { new DateTime(2013, 12, 25), new DateTime(2013, 12, 26), new DateTime(2014, 1, 1) };
+
             var counter = new BusinessDayCounter();
 
             var result = counter.BusinessDaysBetweenTwoDates(startDate, endDate, publicHolidays);
 
-            Assert.Equal(result, expectedResult);
+            Assert.Equal(expectedResult, result);
         }
     }
 
@@ -43,15 +45,13 @@ namespace DateCalculator
         }
     }
 
-    public class BusinessDaysTestData : TheoryData<DateTime, DateTime, IList<DateTime>, int>
+    public class BusinessDaysTestData : TheoryData<DateTime, DateTime, int>
     {
         public BusinessDaysTestData()
         {
-            var publicHolidays = new List<DateTime> { new DateTime(2013, 12, 25), new DateTime(2013, 12, 26), new DateTime(2014, 1, 1) };
-
-            Add(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), publicHolidays, 1);
-            Add(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27), publicHolidays, 0);
-            Add(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), publicHolidays, 59);
+            Add(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), 1);
+            Add(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27), 0);
+            Add(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), 59);
         }
     }
 }
