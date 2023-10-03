b16a6285ed113863510e592d46c1c5b3c8292287
Eric Studley <studley11@live.com.au>
2023-10-03 18:23:32 +1100

    Added business day unit tests

--- DateCalculator/BusinessDayCounterTests.cs
+++ DateCalculator/BusinessDayCounterTests.cs
@@ -10,7 +10,7 @@ namespace DateCalculator
     public class BusinessDayCounterTests
     {
         [Theory]
-        [ClassData(typeof(BusinessDayTestData))]
+        [ClassData(typeof(WeekdaysBetweenTwoDatesTestsData))]
         public void WeekdaysBetweenTwoDatesTests(DateTime startDate, DateTime endDate, int expectedResult)
         {
             var counter = new BusinessDayCounter();
@@ -19,16 +19,39 @@ namespace DateCalculator
 
             Assert.Equal(result, expectedResult);
         }
+
+        [Theory]
+        [ClassData(typeof(BusinessDaysTestData))]
+        public void BusinessDaysBetweenTwoDatesTests(DateTime startDate, DateTime endDate, IList<DateTime> publicHolidays, int expectedResult)
+        {
+            var counter = new BusinessDayCounter();
+
+            var result = counter.BusinessDaysBetweenTwoDates(startDate, endDate, publicHolidays);
+
+            Assert.Equal(result, expectedResult);
+        }
     }
 
-    public class BusinessDayTestData : TheoryData<DateTime, DateTime, int>
+    public class WeekdaysBetweenTwoDatesTestsData : TheoryData<DateTime, DateTime, int>
     {
-        public BusinessDayTestData()
+        public WeekdaysBetweenTwoDatesTestsData()
         {
             Add(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), 1);
             Add(new DateTime(2013, 10, 5), new DateTime(2013, 10, 14), 5);
             Add(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), 61);
             Add(new DateTime(2013, 10, 7), new DateTime(2013, 10, 5), 0);
+        }
+    }
+
+    public class BusinessDaysTestData : TheoryData<DateTime, DateTime, IList<DateTime>, int>
+    {
+        public BusinessDaysTestData()
+        {
+            var publicHolidays = new List<DateTime> { new DateTime(2013, 12, 25), new DateTime(2013, 12, 26), new DateTime(2014, 1, 1) };
+
+            Add(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), publicHolidays, 1);
+            Add(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27), publicHolidays, 0);
+            Add(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), publicHolidays, 59);
         }
     }
 }
