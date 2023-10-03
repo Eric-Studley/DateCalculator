2458b86b70645062d17cc5f73ead6841031cb36e
Eric Studley <studley11@live.com.au>
2023-10-03 17:55:34 +1100

    Add project files.

---
+++ DateCalculator/BusinessDayCounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateCalculator
+{
+    public class BusinessDayCounter
+    {
+        public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        {
+            if (secondDate <= firstDate) return 0;
+
+            return 0;
+            //todo
+        }
+        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
+        {
+            //todo
+            return 0;
+        }
+    }
+}
